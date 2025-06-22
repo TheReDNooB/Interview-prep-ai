392f7baa77a5543a220fb94589a2448aa75cc9e5
TheReDNooB <xmiguelx209@gmail.com>
2025-06-22 18:18:27 -0500

    feat(LandingPage.jsx): add LuSparkles icon import

--- frontend/interview-prep-ai/src/pages/LandingPage.jsx
+++ frontend/interview-prep-ai/src/pages/LandingPage.jsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import HERO_IMG from "../assets/hero-test.png";
 import { APP_FEATURES } from "../utils/data";
 import { useNavigate } from "react-router-dom";
+import { LuSparkles } from 'react-icons/lu'
 
 const LandingPage = () => {
 
@@ -13,51 +14,54 @@ const LandingPage = () => {
   const handleCTA = () => {};
 
   return (
-    <div className="w-full min-h-full bg-[#fffcef]">
-      <div className="w-[500px] h-[500px] bg-amber-200/20 blur-[65px] absolute top-0 left-0"/>
-        <div className="container mx-auto px-4 pt-6 pb-[200] relative z-10">
-          {/* Header */}
-          <header className="flex justify-between items-center mb-16">
-            <div className="text-xl text-black font-bold">
-              Interview Prep AI
-            </div>
-            <button className="bg-linear-to-r from-[#ff9324] to-[#e99a4b] text-sm font-semibold text-white px-7 py-2.5 rounded-full hover:bg-black hover:text-white border border-white transition-colors cursor-pointer" 
-              onClick={() => setOpenAuthModal(true)}>
-              Login / SignUp
-            </button>
-          </header>
-          {/* Hero Content */}
-          <div className="flex flex-col md:flex-row items-center">
-            <div className="w-full md:w-1/2 pr-4 mb-8 md:mb-0">
-              <div className="flex items-center justify-left mb-2">
-                <div className="flex items-center gap-2 text-[13px] text-amber-600 font-semibold bg-amber-100 px-3 py-1 rounded-full border border-amber-300">
-                  AI Powered
-                </div>
-              </div>
-              <h1 className="text-5xl text-black font-medium mb-6 leading-tight">
-                Ace Interview with <br/>
-                <span className="text-transparent bg-clip-text bg-[radial-gradient(circle,_#ff9324_0%,_#fcd760_100%)] bg-[length:200%_200%] animate-text-shine font-semibold">
-                  AI-Powered
-                </span>{" "}
-                Learning
-              </h1>
-            </div>
 
-            <div className="w-full md:w-1/2">
-              <p className="text-[17px] text-gray-900 mr-0 md:mr-20 mb-6">
-                Get role specifi questions, expand answers when you need them,
-                dive deeper into concepts, and organize everything in you way.
-                From preparation from to mastery - your ultimate interview toolkit
-                is here.
-              </p>
-              <button className="bg-black text-sm font-semibold text-white px-7 py-2.5 rounded-full hover:bg-yellow-100 hover:text-black border border-yellow-50 hover:border-yellow-600 transition-colors cursor-pointer" onClick={handleCTA}>
-                Get started
+    <>
+      <div className="w-full min-h-full bg-[#fffcef]">
+        <div className="w-[500px] h-[500px] bg-amber-200/20 blur-[65px] absolute top-0 left-0"/>
+          <div className="container mx-auto px-4 pt-6 pb-[200] relative z-10">
+            {/* Header */}
+            <header className="flex justify-between items-center mb-16">
+              <div className="text-xl text-black font-bold">
+                Interview Prep AI
+              </div>
+              <button className="bg-linear-to-r from-[#ff9324] to-[#e99a4b] text-sm font-semibold text-white px-7 py-2.5 rounded-full hover:bg-black hover:text-white border border-white transition-colors cursor-pointer" 
+                onClick={() => setOpenAuthModal(true)}>
+                Login / SignUp
               </button>
+            </header>
+            {/* Hero Content */}
+            <div className="flex flex-col md:flex-row items-center">
+              <div className="w-full md:w-1/2 pr-4 mb-8 md:mb-0">
+                <div className="flex items-center justify-left mb-2">
+                  <div className="flex items-center gap-2 text-[13px] text-amber-600 font-semibold bg-amber-100 px-3 py-1 rounded-full border border-amber-300">
+                    <LuSparkles/>  AI Powered
+                  </div>
+                </div>
+                <h1 className="text-5xl text-black font-medium mb-6 leading-tight">
+                  Ace Interview with <br/>
+                  <span className="text-transparent bg-clip-text bg-[radial-gradient(circle,_#ff9324_0%,_#fcd760_100%)] bg-[length:200%_200%] animate-text-shine font-semibold">
+                    AI-Powered
+                  </span>{" "}
+                  Learning
+                </h1>
+              </div>
+
+              <div className="w-full md:w-1/2">
+                <p className="text-[17px] text-gray-900 mr-0 md:mr-20 mb-6">
+                  Get role specifi questions, expand answers when you need them,
+                  dive deeper into concepts, and organize everything in you way.
+                  From preparation from to mastery - your ultimate interview toolkit
+                  is here.
+                </p>
+                <button className="bg-black text-sm font-semibold text-white px-7 py-2.5 rounded-full hover:bg-yellow-100 hover:text-black border border-yellow-50 hover:border-yellow-600 transition-colors cursor-pointer" onClick={handleCTA}>
+                  Get started
+                </button>
+              </div>
             </div>
           </div>
-        </div>
       </div>
-  )
-}
+    </>
+  );
+};
 
 export default LandingPage;
