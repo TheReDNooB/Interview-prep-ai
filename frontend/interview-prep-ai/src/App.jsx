d261e93915cef603c0dd31098324fa37b1119f5f
TheReDNooB <xmiguelx209@gmail.com>
2025-06-21 21:19:37 -0500

    feat(app.jsx): import and set up routes

--- frontend/interview-prep-ai/src/App.jsx
+++ frontend/interview-prep-ai/src/App.jsx
@@ -1,8 +1,36 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+
+import Login from "./pages/Auth/Login";
+import SignUp from "./pages/Auth/SignUp";
+import LadingPage from "./pages/LandingPage";
+import Dashboard from "./pages/Home/Dashboard";
+import InterviewPrep from "./pages/InterviewPrep/InterviewPrep";
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Router>
+        <Routes>
+          {/* Default routes */}
+          <Route path="/" element={<LadingPage/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/signUp" element={<SignUp/>}/>
+          <Route path="/dashboard" element={<Dashboard/>}/>
+          <Route path="/interview-prep/:sessionId" element={<InterviewPrep/>}/>
+        </Routes>
+      </Router>
+
+      <Toaster>
+        toastOptions={{
+          className: '',
+          style: {
+            fontSize: '13px',
+          },
+        }}
+      </Toaster>
+    </div>
   )
 }
 
