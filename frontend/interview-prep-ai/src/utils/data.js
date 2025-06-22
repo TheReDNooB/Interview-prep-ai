5459b8a764fce483ce3e32c189ef6056b0218cf8
TheReDNooB <xmiguelx209@gmail.com>
2025-06-21 21:46:50 -0500

    feat(LandingPage.jsx): create and style landing page

---
+++ frontend/interview-prep-ai/src/utils/data.js
@@ -0,0 +1,33 @@
+
+export const APP_FEATURES = [
+  {
+    id: "01",
+    title: "Tailored just for you",
+    description: "Get interview questions and model answers base on you role"
+  },
+  {
+    id: "02",
+    title: "AI-Powered",
+    description: "Get instant feedback on your answers with AI-powered analysis"
+  },
+  {
+    id: "03",
+    title: "Track your progress",
+    description: "Monitor your performance and improve over time with detailed analytics"
+  },
+  {
+    id: "04",
+    title: "Practice anytime, anywhere",
+    description: "Access the platform from any device and practice at your own pace"
+  },
+  {
+    id: "05",
+    title: "Expert tips and resources",
+    description: "Access a wealth of resources and expert tips to help you succeed"
+  },
+  // {
+  //   id: "06",
+  //   title: "Community support",
+  //   description: "Join a community of learners and get support from peers and mentors"
+  // }
+];
