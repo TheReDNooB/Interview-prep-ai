807982715cd588d035f27abd5a4d8c39977450c2
TheReDNooB <xmiguelx209@gmail.com>
2025-06-22 17:10:57 -0500

    feat(LandingPage.jsx): adjust navigation on the landing page

--- frontend/interview-prep-ai/src/pages/LandingPage.jsx
+++ frontend/interview-prep-ai/src/pages/LandingPage.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 import HERO_IMG from "../assets/hero-test.png";
 import { APP_FEATURES } from "../utils/data";
+import { useNavigate } from "react-router-dom";
 
 const LandingPage = () => {
+
+  const navigate = useNavigate();
+
+  const [openAuthModal, setOpenAuthModal] = useState(false);
+  const [currentPage, setCurrentPage] = useState("login");
+  const handleCTA = () => {};
+
   return (
     <div>LandingPage</div>
   )
