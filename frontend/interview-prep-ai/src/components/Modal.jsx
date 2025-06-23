75a87c32b13ba181f8a5ff0944342b8b70828f93
TheReDNooB <xmiguelx209@gmail.com>
2025-06-22 21:50:01 -0500

    feat(Modal.jsx): apply styles to the modal component

--- frontend/interview-prep-ai/src/components/Modal.jsx
+++ frontend/interview-prep-ai/src/components/Modal.jsx
@@ -1,23 +1,23 @@
 import React from "react";
 
 const Dashboard = ({ children, isOpen, onClose, title, hideHeader }) => {
-  return <div className="">
+  return <div className="fixed inset-0 flex justify-center items-center w-full h-full bg-black/40">
     {/* Modal Content */}
     <div className={`relative flex flex-col bg-white shadow-lg rounded-lg overflow-hidden`}>
     {/* Modal Header */}
     {!hideHeader && (
-      <div className="">
-        <h3 className="">{title}</h3>
+      <div className="flex items-center justify-between p-4 border-b border-gray-200">
+        <h3 className="md:text-lg font-medium text-shadow-gray-900">{title}</h3>
       </div>
     )}
 
     <button
     type="button"
-    className=""
+    className="text-gray-400 bg-transparent hover:bg-orange-100 hover:text-gray-900 rounded-lg text-sm w-8 h-8 flex justify-center items-center absolute top-3.5 right-3.5 cursor-pointer"
     onClick={onClose}
     >
       <svg
-      className=""
+      className="w-3 h-3"
       aria-hidden="true"
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
@@ -33,10 +33,10 @@ const Dashboard = ({ children, isOpen, onClose, title, hideHeader }) => {
       </svg>
     </button>
 
-    {/* Modal Body Scrollable */}
-    <div className="">
-      {children}
-    </div>
+      {/* Modal Body Scrollable */}
+      <div className="text-1 overflow-y-auto custom-scrollbar">
+        {children}
+      </div>
     </div>
   </div>
 }
