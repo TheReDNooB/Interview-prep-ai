22f42e2876dbb92a1969cde10d328382e11a1d57
TheReDNooB <xmiguelx209@gmail.com>
2025-06-23 18:17:15 -0500

    fix(Login.jsx): fix incorrect state variable

--- frontend/interview-prep-ai/src/pages/Auth/Login.jsx
+++ frontend/interview-prep-ai/src/pages/Auth/Login.jsx
@@ -5,7 +5,7 @@ import Input from '../../components/Inputs/Input'
 const Login = ({ setCurrentPage }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
+  const [error, setError] = useState(null);
 
   const navigate = useNavigate();
 
