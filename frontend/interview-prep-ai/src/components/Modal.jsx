eb9474d988fc5d3accf8a30e3fffb2000f244354
TheReDNooB <xmiguelx209@gmail.com>
2025-06-22 21:31:46 -0500

    feat(Modal.jsx): create modal component

---
+++ frontend/interview-prep-ai/src/components/Modal.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Dashboard = ({ children, isOpen, onClose, title, hideHeader }) => {
+  return <div className="">
+    {/* Modal Content */}
+    <div className={`relative flex flex-col bg-white shadow-lg rounded-lg overflow-hidden`}>
+    {/* Modal Header */}
+    </div>
+  </div>
+}
+
+export default Dashboard;
