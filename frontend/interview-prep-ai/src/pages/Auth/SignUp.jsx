50ce764542f8a789a64c172c1eff0aea3fb33609
TheReDNooB <xmiguelx209@gmail.com>
2025-06-23 20:51:12 -0500

    feat(SignUp.jsx): implement signup component

--- frontend/interview-prep-ai/src/pages/Auth/SignUp.jsx
+++ frontend/interview-prep-ai/src/pages/Auth/SignUp.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Input from '../../components/Inputs/Input'
 
-const SignUp = () => {
-  return (
-    <div>SignUp</div>
-  )
-}
+const SignUp = ({setCurrentPage}) => {
+  const [profilePic, setProfilePic] = useState(null);
+  const [fullName, setFullName] = useState("");
+  const [Email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  // handle sigup form submit
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+  }
+
+  return <div>SignUp</div>
+
+};
 
 export default SignUp;
