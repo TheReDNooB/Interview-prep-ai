ade0428993115d753dbd902d4f7290dd0e5b8848
TheReDNooB <xmiguelx209@gmail.com>
2025-06-23 21:06:46 -0500

    feat(SignUp.jsx): implement input fields in signup modal

--- frontend/interview-prep-ai/src/pages/Auth/SignUp.jsx
+++ frontend/interview-prep-ai/src/pages/Auth/SignUp.jsx
@@ -16,7 +16,54 @@ const SignUp = ({setCurrentPage}) => {
     e.preventDefault();
   }
 
-  return <div>SignUp</div>
+  return <div className="">
+    <h3 className="">Create an Account</h3>
+    <p className="">
+      Join us today by entering your details below
+    </p>
+
+    <form onSubmit={handleSignUp}>
+      <div className="">
+        <Input
+          value={fullName}
+          onChange={({ target }) => setFullName(target.value)}
+          label="Fullname"
+          placeholder="Steve"
+          type="text"
+        />
+        <Input
+          value={Email}
+          onChange={({ target }) => setEmail(target.value)}
+          label="Email Address"
+          placeholder="hello@example.com"
+          type="text"
+        />
+        <Input
+          value={password}
+          onChange={({ target }) => setPassword(target.value)}
+          label="Password"
+          placeholder="Min 8 Characters"
+          type="password"
+        />
+      </div>
+
+      {error && <p className="">{error}</p>}
+      <button type="" className="">
+        Sign Up
+      </button>
+      <p className="">
+        Already an Account?{" "}
+        <button 
+          className=""
+          onClick={() => {
+            setCurrentPage("login");
+          }}>
+          Login
+        </button>
+      </p>
+    </form>
+
+  </div>
 
 };
 
