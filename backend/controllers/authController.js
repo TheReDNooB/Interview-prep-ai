b9cc8a54aef0fb2c09d49e089242e18092a93786
TheReDNooB <xmiguelx209@gmail.com>
2025-06-27 16:58:20 -0500

    feat(authController.js): implement user registration endpoint
    Implemented the user registration endpoint.
    
    Features include:
    - Checking for existing users to prevent duplicates.
    - Hashing user passwords for security.
    - Registering the new user in the database.

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -11,6 +11,38 @@ const generateToken = (userid) => {
 // @route   POST /api/auth/register
 // @access  Public
 const registerUser = async (req, res) => {
+  try {
+    const { name, email, password, profileImageUrl } = req.body;
+
+    // check if user already exists
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" })
+    }
+
+    // hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // create new user
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      profileImageUrl
+    });
+
+    // return user data and token
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      profileImageUrl: user.profileImageUrl,
+      token: generateToken(user._id),
+    });
+  } catch (error) {
+    res.status(500).json({ message: "server error", error: error.message });
+  }
 };
 
 // @desc    login a user
