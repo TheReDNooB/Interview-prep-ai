e2aa9b8a33fa6c710b8d46df4c812d6a21c0656e
TheReDNooB <xmiguelx209@gmail.com>
2025-06-27 15:00:16 -0500

    feat(User.js): set up the session schema for the database

---
+++ backend/models/Session.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const sessionSchema = new mongoose.Schema({
+  user : { type:mongoose.Schema.Types.ObjectId, ref: 'User' },
+  role : { type: String, required: true },
+  experience : { type: String, required: true },
+  topicFocus : { type: String, required: true },
+  descrition: String,
+  questions: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Question' }],
+},
+{
+  timestamps: true,
+});
+
+module.exports = mongoose.model('Session', sessionSchema);
