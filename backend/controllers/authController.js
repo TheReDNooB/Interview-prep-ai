2734538f7602f85948954202c6b53b832deabe10
TheReDNooB <xmiguelx209@gmail.com>
2025-07-02 17:13:52 -0500

    fix(authController.js): fix grammatical error

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -82,7 +82,7 @@ const loginUser = async (req, res) => {
 // @access  Private
 const getUserProfile = async (req, res) => {
   try {
-    const user = await User.findById(req.user._id).select("-pasword");
+    const user = await User.findById(req.user._id).select("-password");
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
