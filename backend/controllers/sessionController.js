30008e79f43bf51ed956424dbb9c50d4dfd7f503
TheReDNooB <xmiguelx209@gmail.com>
2025-07-04 15:25:22 -0500

    feat(sessionController.js): add controller to delete sessions

--- backend/controllers/sessionController.js
+++ backend/controllers/sessionController.js
@@ -69,6 +69,23 @@ exports.getSessionById = async (req, res) => {
 // @access  Private
 exports.deleteSession = async (req, res) => {
   try {
+    const session = await Session.findById(req.params.id);
+    if (!session) {
+      return res.status(404).json({ success: false, messsage: "Session not found" });
+    }
+
+    // check if the session belong to the logged-in user
+    if (session.user.toString() !== req.user._id.toString()) {
+      return res.status(401).json({ success: false, message: "Not authorized  to delete this session" });
+    }
+
+    // delete all questions linked to the session
+    await Question.deleteMany({ session: session._id });
+
+    // delete the session
+    await session.deleteOne();
+
+    return res.status(200).json({ success: true, message: "Session deleted successfully" })
   } catch (error) {
     res.status(500).json({ success: false, message: "Server Error" })
   }
